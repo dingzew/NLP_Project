dca9e0097dace0174f3524023b4d4e30ea386622
Weihan Li <lwh990316@gmail.com>
2018-11-11 12:53:10 -0500

    answer file with tf-idf

--- answer.py
+++ answer.py
@@ -6,6 +6,10 @@ import string
 import os
 import re
 import sys
+import math
+
+# nltk.download('punkt')
+# nltk.download('stopwords')
 
 class Answer(object):
     def __init__(self, sentences, question):
@@ -18,12 +22,12 @@ class Answer(object):
 
         self.sentences = self.preprocessSentence(self.sentences)
         self.questionDict = self.compQues(self.question)
-        self.sentDict = self.calculToken(self.sentences)
-        self.vectorList = self.similarity(self.sentDict, self.question)
+        self.sentDict = self.calculToken(self.sentences, self.questionDict)
+        self.vectorList = self.similarity(self.question)
 
     def preprocessSentence(self, sentList):
         for words in sentList:
-            words = [w for w in words if w not in stopwords and not w.isdigit()]
+            words = [w for w in words if w not in stopwords.words()]
         return sentList
 
     # take in a sentence of words
@@ -34,8 +38,10 @@ class Answer(object):
 
         for words in sentList:
             d = dict()
-            for word in questionSet:
-                d[word] = d.get(word, 0) + 1
+
+            for word in words:
+                if word in questionDict:
+                    d[word] = d.get(word, 0) + 1
             for word in d:
                 d[word] = d[word] / self.length
             sentDictList.append(d)
@@ -48,7 +54,7 @@ class Answer(object):
         return d
 
     def termFreq(self, sentDict, word):
-        return sentDict[word]
+        return sentDict.get(word, 0)
 
     def inverseFreq(self, word, sentDict):
         count = 0
@@ -58,7 +64,7 @@ class Answer(object):
 
     def similarity(self, question):
         vectorList = []
-        for sent in self.sentDictList:
+        for sent in self.sentDict:
             vector = []
             for word in question:
                 tfidf = self.termFreq(sent, word) * self.inverseFreq(word, sent)
@@ -66,9 +72,19 @@ class Answer(object):
             vectorList.append(vector)
         return vectorList
 
-with open ("data/set1/a1/txt", "r") as doc:
-    text = doc.read()
-    sent_text = nltk.sent_tokenize(text)
-    for sentence in sent_text:
-        tokenized_text = nltk.word_tokenize(sentence)
-        tagged = nltk.pos_tag(tokenized_text) 
+    def getSentence(self):
+        for vector in self.vectorList:
+            pass
+
+#with open ("data/set1/a2.txt", "r") as doc:
+#text = doc.read()
+text = "Early life and education\nDonovan was born on March 4, 1982, in Ontario, California, to Donna Kenney-Cash, a special education teacher, and Tim Donovan, a semi-professional ice hockey player originally from Canada, which makes Donovan a Canadian citizen by descent. His mother raised him and his siblings in Redlands, California.\nWhen Donovan was six, his mother allowed him to join an organized league, and he scored seven goals in his first game. Donovan was a member of Cal Heat – a club based in nearby Rancho Cucamonga under coach Clint Greenwood. In 1997, he was accepted into U.S. Youth Soccer's Olympic Development Program. He attended Redlands East Valley High School when not engaged in soccer activities elsewhere. In 1999, Donovan attended the IMG Academy in Bradenton, Florida, part of U.S. Soccer's training program."
+sent_text = nltk.sent_tokenize(text)
+token = []
+for sentence in sent_text:
+    tokenized_text = nltk.word_tokenize(sentence)
+    token.append(tokenized_text)
+question = "Who was born in California".split()
+A = Answer(token, question)
+
+        # tagged = nltk.pos_tag(tokenized_text) 
