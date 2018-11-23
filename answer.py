30baf6f140f69918bfcb0b7ac69f2537af1a739b
wyq <wyq@apples-MBP-2.wv.cc.cmu.edu>
2018-11-23 17:51:31 -0500

    modify answer.py

--- answer.py
+++ answer.py
@@ -14,33 +14,30 @@ import math
 from nltk.stem.wordnet import WordNetLemmatizer
 import answer_yesno
 from process import *
+from tree_file import *
 # nltk.download('punkt')
 # nltk.download('stopwords')
 lemma = WordNetLemmatizer()
+VERB = ["VB","VBZ","VBP","VBD","VBN","VBG"]
 
 class Answer(object):
     def __init__(self, orig, sentences):
         self.original = orig
-        
         self.length = 0
-        
         self.sentences = self.preprocessSentence(sentences) # tokenized
     
     def setQuestion(self, question):
-        self.question = question
+        self.raw_question = question
         self.tag = getTag(question)
-        for s in self.sentences:
-            for word in self.question:
-                self.length += s.count(word)
+        self.question = re.compile('\w+').findall(question.lower())
         self.questionSynset = self.preprocessQuestion()
         self.questionDict = self.compQues(self.question)
         self.sentDict = self.calculToken(self.sentences, self.questionDict)
         self.vectorList = self.similarity(self.question)
 
     def preprocessSentence(self, sentList):
-        for words in sentList:
-            words = [w for w in words if w not in stopwords.words()]
-            words = [lemma.lemmatize(w) for w in words]
+        for index,words in enumerate(sentList):
+            sentList[index] = [lemma.lemmatize(w) for w in words if w not in stopwords.words()]
         return sentList
 
     def preprocessQuestion(self):
@@ -61,7 +58,7 @@ class Answer(object):
                 if word in questionDict:
                     d[word] = d.get(word, 0) + 1
             for word in d:
-                d[word] = d[word] / self.length
+                d[word] = d[word] / len(words)
             sentDictList.append(d)
         return sentDictList
 
@@ -74,38 +71,55 @@ class Answer(object):
     def termFreq(self, sentDict, word):
         return sentDict.get(word, 0)
 
-    def inverseFreq(self, word, sentDict):
+    def inverseFreq(self, word):
         count = 0
-        if word in sentDict:
-            count += 1
+        for sent in self.sentDict:
+            if word in sent:
+                count += 1
         return math.log(len(self.sentences) / (count + 1))
 
     def similarity(self, question):
+        tokens = nltk.word_tokenize(self.raw_question)
+        path_to_model = "stanford-ner/classifiers/english.muc.7class.distsim.crf.ser.gz"
+        path_to_jar = "stanford-ner/stanford-ner.jar"
+        st = StanfordNERTagger(path_to_model, path_to_jar)
+        question = st.tag(tokens)
+#        print(question)
         vectorList = []
         for sent in self.sentDict:
             vector = []
-            for word in question:
-                tfidf = self.termFreq(sent, word) * self.inverseFreq(word, sent)
+            for word,tag in question:
+                word = word.lower()
+                tfidf = self.termFreq(sent, word) * self.inverseFreq(word)
+                if tag != 'O':
+                    tfidf *= 2
                 vector.append(tfidf)
             vectorList.append(vector)
         return vectorList
 
     def getSentence(self):
-        max_sum = 0
-        max_index = 0
+        score = []
         for j in range(len(self.vectorList)):
             vector = self.vectorList[j]
-            curr_sum = sum([i**2 for i in vector])
-            if curr_sum > max_sum:
-                max_sum = curr_sum
-                max_index = j
-        return max_index
+            curr_sum = sum([i for i in vector])
+            score.append((curr_sum, j))
+            score.sort(key = lambda x : x[0], reverse=True)
+        j = 0
+        while True:
+            res = []
+            parsed_ques = generateTree(self.original[score[j][1]])
+            for tag in VERB:
+                res += get_phrases(parsed_ques, tag)
+            if len(res) > 0:
+                break
+            j += 1
+        return score[j][1]
         # get the highest ranked sentences
 
     def answer_yesno(self):
         target_index = self.getSentence()
         target = self.original[target_index]
-        answer = answer_yesno.get_ans_wrapper(self.question, target)
+        answer = answer_yesno.get_ans_wrapper(self.raw_question, target)
         if answer:
             return "Yes."
         else:
@@ -117,6 +131,7 @@ class Answer(object):
         return target
 
     def get_answer(self):
+        return self.answer_wh()
         if self.tag == "YESNO":
             return self.answer_yesno()
         else:
@@ -152,14 +167,10 @@ def main(argv):
 
     sent_text = nltk.sent_tokenize(article)
     sent_text = [s for s in sent_text]
-    token_list = [] 
+    token_list = []
     for sentence in sent_text:
         tokenized_text = nltk.word_tokenize(sentence)
-        res = []
-        for w in tokenized_text:
-            
-            # w = re.findall(r'[\w.].*[\w.]', s)[0]
-            res.append(w.lower())
+        res = [w.lower() for w in tokenized_text]
 
         token_list.append(res)
 
@@ -171,12 +182,8 @@ def main(argv):
     question_list = question.split("\n")
     A = Answer(sent_text, token_list)
     for ques in question_list:
-        #try:
-        ques = re.findall(r'[\w.].*[\w.]', ques)[0]
-        A.setQuestion(ques.lower())
+        A.setQuestion(ques)
         print (A.get_answer())
-        # except:
-            # print ("NULL")
 
     art_doc.close()
     ques_doc.close()
